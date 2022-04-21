f1786f3645efc64d19857e7b1dee72cd29f67214
Chase Lukasiewicz <chaseluka@gmail.com>
2022-04-21 12:08:47 -0600

    script.js

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
+const container = document.querySelector('.container');
 let library = [];
+
 
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
+    this.info = function(){
+        return (title + ' by ' + author + ', ' + pages + ' pages, ' + read);
+    }
 }
 
 function addBooktoLibrary(){
     for(let i = 0; i < 2; i++){
         const newBook = new Book(prompt('Title'), prompt('Author'), prompt('Pages'), prompt('Read'));
+        const div = document.createElement('div');
+        div.textContent = newBook.info();
+        container.appendChild(div);
         library.push(newBook);
     }
     console.log(library);
 }
 
 addBooktoLibrary();
-
-function displayBook(){
-    let books = library.length;
-
-    for(let i = 0; i <= books; i++){
-        console.log(library[0 + i]);
-    }
-}
-
-displayBook();
