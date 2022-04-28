d27e7b50a84705a5dd4ae3b4d55ceb7666accc38
Chase Lukasiewicz <chaseluka@gmail.com>
2022-04-28 16:28:18 -0600

    Delete Book from library/touch up

--- script.js
+++ script.js
@@ -1,49 +1,78 @@
-const container = document.querySelector('.container');
+//Collection of variables and arrays for later use.
+
 const tbody = document.querySelector('tbody');
 const btn = document.getElementById('btn');
+
+//Array storing all books
+
 let library = [];
 
+//Book object constructor for being used infinitely
 
-function Book(title, author, pages, read) {
+function book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-        return (title + ' by ' + author + ', ' + pages + ' pages, ' + read);
-    }
 }
 
+//Adds a new book to the library array and updates it to the table.
+
 function addBooktoLibrary(){
-    const newBook = new Book(
+    const newBook = new book(  //Allow form input to become a new book
         document.getElementById('title').value, 
         document.getElementById('author').value, 
         document.getElementById('pages').value,
-        document.getElementById('read').checked
+        document.getElementById('read').checked //To return a boolean
         );
+
+    //Add new book to the DOM with correct values from newBook
+  
     const row = document.createElement('tr');
+    row.setAttribute('data-title', `${newBook.title}`);
     tbody.appendChild(row);
-    
+
     const tdTitle = document.createElement('td');
     const tdAuthor = document.createElement('td');
     const tdPages = document.createElement('td');
     const tdRead = document.createElement('td');
+    const tdDelete = document.createElement('td');
+
+    tdDelete.classList.add('delete-book');
+
+    if (newBook.read === true){
+        tdRead.textContent = "Read";
+        tdRead.classList.add('read');
+        tdRead.classList.add('read-status');
+    }
+    else {
+        tdRead.textContent = "Not Read";
+        tdRead.classList.add('not-read');
+        tdRead.classList.add('read-status');
+    }
     
     tdTitle.textContent = newBook.title;
     tdAuthor.textContent = newBook.author;
     tdPages.textContent = newBook.pages;
-    tdRead.textContent = newBook.read;
 
     row.appendChild(tdTitle);
     row.appendChild(tdAuthor);
     row.appendChild(tdPages);
     row.appendChild(tdRead);
+    row.appendChild(tdDelete);
 
-    library.push(newBook);
+    const img = document.createElement('img'); //Create minus circle
+    img.src = 'images/minus-circle.svg';
+    tdDelete.appendChild(img);
+
+    library.push(newBook);  //store newBook into the array
+    
+    tdDelete.addEventListener('click', () => deleteBook(tdDelete))
 }
 
-btn.addEventListener('click', addBooktoLibrary);
+btn.addEventListener('click', addBooktoLibrary); //add the new book to the library when submitted
 
+//Allows form to display when buttons are clicked.
 
 function on() {
     document.getElementById("form").style.display = "block";
@@ -52,3 +81,14 @@ function on() {
 function off() {
     document.getElementById("form").style.display = "none";
 };
+
+//Deletes a book from library when minus button is clicked.
+
+function deleteBook (tdDelete){
+    let bookTitle = tdDelete.parentElement.getAttribute('data-title'); //target parent element of title selected given it has a prestablished data-title
+    library = library.filter(obj => {  //filter through array and find all titles, if one matches data-title from above, returns the array without this item.
+        console.log(obj.title);
+        return obj.title != bookTitle;
+    })
+    tdDelete.parentElement.remove(); //Removes from display
+}
